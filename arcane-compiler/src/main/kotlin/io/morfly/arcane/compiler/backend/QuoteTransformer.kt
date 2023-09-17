f3644153d4fafff6d8d0f55da7a529abb46b2911
Pavlo Stavytskyi <pavlo.stavytskyi@gmail.com>
2023-09-17 15:54:14 -0700

    splices improved integration

--- arcane-compiler/src/main/kotlin/io/morfly/arcane/compiler/backend/QuoteTransformer.kt
+++ arcane-compiler/src/main/kotlin/io/morfly/arcane/compiler/backend/QuoteTransformer.kt
@@ -2,20 +2,21 @@ package io.morfly.arcane.compiler.backend
 
 import org.jetbrains.kotlin.backend.common.extensions.IrPluginContext
 import org.jetbrains.kotlin.backend.common.lower.DeclarationIrBuilder
-import org.jetbrains.kotlin.ir.SourceRangeInfo
+import org.jetbrains.kotlin.ir.IrElement
 import org.jetbrains.kotlin.ir.backend.js.utils.valueArguments
 import org.jetbrains.kotlin.ir.builders.*
 import org.jetbrains.kotlin.ir.declarations.IrFile
 import org.jetbrains.kotlin.ir.declarations.path
 import org.jetbrains.kotlin.ir.expressions.*
-import org.jetbrains.kotlin.ir.util.*
+import org.jetbrains.kotlin.ir.util.dump
+import org.jetbrains.kotlin.ir.util.fqNameWhenAvailable
+import org.jetbrains.kotlin.ir.util.getSimpleFunction
+import org.jetbrains.kotlin.ir.util.irCall
 import org.jetbrains.kotlin.ir.visitors.IrElementTransformer
 import org.jetbrains.kotlin.name.ClassId
 import org.jetbrains.kotlin.name.FqName
 import org.jetbrains.kotlin.name.Name
 import java.io.File
-import java.lang.StringBuilder
-import kotlin.math.exp
 
 
 const val RUNTIME_PACKAGE = "io.morfly.arcane.runtime"
@@ -28,6 +29,9 @@ class QuoteTransformer(
 ) : IrElementTransformer<QuoteTransformer.Data?> {
     private val fileSource: String = File(irFile.path).readText()
 
+    private val stringType = pluginContext.irBuiltIns.stringType
+    private val charType = pluginContext.irBuiltIns.charType
+
     private val quoteType = pluginContext
         .referenceClass(ClassId(FqName(RUNTIME_PACKAGE), Name.identifier("Quote")))!!
 
@@ -38,7 +42,7 @@ class QuoteTransformer(
         irFile.transformChildren(transformer = this, data = null)
     }
 
-    override fun visitCall(expression: IrCall, data: Data?): IrExpression {
+    override fun visitCall(expression: IrCall, data: Data?): IrElement {
         when (expression.symbol.owner.fqNameWhenAvailable) {
             QUOTE_FQ_NAME -> {
                 val quoteData = when (data) {
@@ -60,12 +64,10 @@ class QuoteTransformer(
                     if (body != null) {
                         data.splices += expression
                     }
-                } else {
-                    error("TODO")
                 }
             }
         }
-        return expression
+        return super.visitCall(expression, data)
     }
 
     private fun lowerQuote(expression: IrFunctionExpression, data: QuoteData) {
@@ -80,28 +82,10 @@ class QuoteTransformer(
             data.splices.forEachIndexed { i, splice: IrCall ->
                 val spliceRangeInfo = irFile.fileEntry.getSourceRangeInfo(splice.startOffset, splice.endOffset)
                 concat.addArgument(irString(fileSource.substring(lastOffset, spliceRangeInfo.startOffset)))
-                concat.addArgument(irCall(splice, splice.symbol))
+                concat.addArgument(lowerSplice(splice))
                 lastOffset = spliceRangeInfo.endOffset
             }
             concat.addArgument(irString(fileSource.substring(lastOffset, rangeInfo.endOffset)))
-
-//            val spliceRangeInfo = data.nestedSplices.first().originalRangeInfo
-//
-//            val concat = irConcat()
-//            val fragment1 = buildString {
-//                append(startIndent)
-//                append(fileSource.substring(rangeInfo.startOffset, spliceRangeInfo.startOffset))
-//            }
-//            concat.addArgument(irString(fragment1))
-//
-//            concat.addArgument(irCall(data.nestedSplices.first().expression, data.nestedSplices.first().expression.symbol))
-//
-//            val fragment2 = buildString {
-//                append(fileSource.substring(spliceRangeInfo.endOffset, rangeInfo.endOffset))
-//            }
-//            concat.addArgument(irString(fragment2))
-//
-//            println("TTAGG concat: ${concat.dump()}")
 
             val callSetter = irCall(quoteCodeSetter!!.owner).apply {
                 dispatchReceiver = irGet(expression.function.extensionReceiverParameter!!)
@@ -114,6 +98,23 @@ class QuoteTransformer(
         body.statements.clear()
         body.statements.addAll(loweredBody.statements)
     }
+
+    private fun IrBuilderWithScope.lowerSplice(splice: IrCall): IrExpression =
+        when (splice.type) {
+            stringType -> irConcat().apply {
+                addArgument(irString("\""))
+                addArgument(irCall(splice, splice.symbol))
+                addArgument(irString("\""))
+            }
+
+            charType -> irConcat().apply {
+                addArgument(irString("'"))
+                addArgument(irCall(splice, splice.symbol))
+                addArgument(irString("'"))
+            }
+
+            else -> irCall(splice, splice.symbol)
+        }
 
     sealed interface Data
 }
--- src/main/kotlin/QuoteTest.kt
+++ src/main/kotlin/QuoteTest.kt
@@ -1,16 +1,26 @@
 import io.morfly.arcane.runtime.quote
 import io.morfly.arcane.runtime.splice
 
+fun getBool() = false
+
 fun test() {
     val code = quote {
 
         val s = splice {
-            5 + 5
-        }
-        val d = splice {
+            -10 + 5
+        } + splice {
             10 * 2
         }
-        s + d
+        val sts = splice {
+            "1" + "2"
+        } + splice { 'a' }
+
+        val ch = splice { 'a' }
+
+        val tr = splice {
+            getBool()
+        }
+        sts + s + splice { "spl" } + tr + ch
     }
     println("TTAGG text: \n${code.text}")
     println("TTAGG value: ${code.evaluate()}")
