c49952a7b4cc41a5df05bc324073e74172895d12
Pavlo Stavytskyi <pavlo.stavytskyi@gmail.com>
2023-11-29 07:28:31 -0800

    update main

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,14 +2,14 @@ import io.morfly.arcane.runtime.quote
 import io.morfly.arcane.runtime.splice
 
 fun main() {
-    val text = "5 + ${5 * 4}"
+    val text = "100 + ${5 * 4}"
     println(text)
 
     val code = quote {
-        5 + splice { 5 * 4 }
+        100 + splice { 5 * 4 }
     }
     println(code.text)
 
-    val value = 5 + run { 5 * 4 }
+    val value = 100 + run { 5 * 4 }
     println(value)
 }
